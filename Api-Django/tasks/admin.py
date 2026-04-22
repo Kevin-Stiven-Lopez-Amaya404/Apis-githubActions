409f9edb01c43bace8085836d8f3d11e886a04fb
mrroobot08-dotcom <mrroobot08@gmail.com>
2026-04-22 07:57:02 -0500

    feat: Add due_date field to Task model

--- Api-Django/tasks/admin.py
+++ Api-Django/tasks/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['title', 'completed', 'due_date']
+    list_filter = ['completed', 'due_date']
+    search_fields = ['title', 'description']
+
+admin.site.register(Task, TaskAdmin)
--- Api-Django/tasks/serializers.py
+++ Api-Django/tasks/serializers.py
@@ -4,4 +4,5 @@ from .models import Task
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = '__all__'
+        fields = ['id', 'title', 'description', 'completed', 'due_date']
+        read_only_fields = ['id']
